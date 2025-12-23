7c5e924ca8c54b859bcbb91f21ef4c6d26dcb6ed
galuh <galuh.romanita@gmail.com>
2025-12-23 22:45:00 +0800

    ya Allah bisa pls

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./frontend/templates/**/*.{html,js}", "./app/routers/**/*.py"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
